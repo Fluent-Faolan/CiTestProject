096cb774955f27d79076f1664c22c98f63149c88
Faolan <faolan.ohare@fluenttechnology.com>
2018-07-03 12:38:05 +0100

    Basic Fuzzy logic for Temperature Summaries

--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CiTestProject.Controllers
@@ -9,8 +8,11 @@ namespace CiTestProject.Controllers
     [Route("api/[controller]")]
     public class SampleDataController : Controller
     {
-        private static string[] Summaries = new[]
-        {
+        private const int MinimumTemp = -20;
+        private const int MaximumTemp = 55;
+        private const int Range = MaximumTemp - MinimumTemp;
+
+        private static readonly string[] Summaries = {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
@@ -18,12 +20,22 @@ namespace CiTestProject.Controllers
         public IEnumerable<WeatherForecast> WeatherForecasts(int startDateIndex)
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+
+            return Enumerable.Range(1, 5).Select(index =>
             {
-                DateFormatted = DateTime.Now.AddDays(index + startDateIndex).ToString("d"),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                var rand = rng.Next(-20, 55);
+                return new WeatherForecast
+                {
+                    DateFormatted = DateTime.Now.AddDays(index + startDateIndex).ToString("d"),
+                    TemperatureC = rand,
+                    Summary = Summaries[RelativeTemp(rand)]
+                };
             });
+        }
+
+        private static int RelativeTemp(int current)
+        {
+            return (int)Math.Floor( (float)( ( current - MinimumTemp ) / (double)Range ) * Summaries.Length);
         }
 
         public class WeatherForecast
@@ -32,10 +44,7 @@ namespace CiTestProject.Controllers
             public int TemperatureC { get; set; }
             public string Summary { get; set; }
 
-            public int TemperatureF
-            {
-                get { return 32 + (int) (TemperatureC / 0.5556); }
-            }
+            public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
         }
     }
 }
